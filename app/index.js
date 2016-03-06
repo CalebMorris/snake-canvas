fb1571075a2f5ea55d6e6c16310c0697942c5b1d
Caleb Morris <caleb.morris.g@gmail.com>
2016-03-06 15:54:29 -0700

    Added fps counter

--- app/index.js
+++ app/index.js
@@ -1,4 +1,45 @@
-var c = document.getElementById('myCanvas');
-var ctx = c.getContext('2d');
-ctx.fillStyle = '#FF0000';
-ctx.fillRect(0, 0, 20, 75);
+let fps = 0;
+let lastRun = null;
+let lastPrint = new Date().getTime();
+
+function showFPS(extra) {
+  const timeSinceLastPrint = (new Date().getTime() - lastPrint);
+  if (extra && timeSinceLastPrint > 500) {
+    extra.textContent = `FPS: ${String(fps.toFixed(2))}`;
+    lastPrint = new Date().getTime();
+  }
+}
+
+(function(window, document) {
+
+  const canvas = document.getElementById('game-canvas');
+  const extra = document.getElementById('extra-content');
+  const context = canvas.getContext('2d');
+  const width = canvas.width;
+  const height = canvas.height;
+
+  const isGameRunning = true;
+  const shouldShowFPS = true;
+
+  function gameLoop() {
+    if (! lastRun) {
+      lastRun = new Date().getTime();
+      window.requestAnimationFrame(gameLoop);
+      return;
+    }
+
+    var delta = (new Date().getTime() - lastRun) / 1000;
+    lastRun = new Date().getTime();
+    fps = 1 / delta;
+
+    // Clear screen
+    context.clearRect(0, 0, width, height);
+
+    if (shouldShowFPS) showFPS(extra);
+
+    if (isGameRunning) window.requestAnimationFrame(gameLoop);
+  }
+
+  gameLoop();
+
+}(window, window.document));
