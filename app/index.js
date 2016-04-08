e910e742ba9aa845e49d22571be3796aa9794924
Caleb Morris <caleb.morris.g@gmail.com>
2016-04-07 21:51:47 -0700

    Updated main view to have title and sections

--- app/index.js
+++ app/index.js
@@ -5,13 +5,13 @@ require('./Assets/style.scss');
 (function(window, document) {
 
   const canvas = document.getElementById('game-canvas');
-  const extra = document.getElementById('extra-content');
+  const fpsLabel = document.getElementById('fps');
   const context = canvas.getContext('2d');
 
   const gameController = new GameController(canvas, context, canvas.width, canvas.height, window.requestAnimationFrame);
 
   setInterval(() => {
-    extra.textContent = gameController.fps.toFixed(2);
+    fpsLabel.textContent = gameController.fps.toFixed(2);
   }, 500);
 
   gameController.start();
